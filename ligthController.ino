6b77caf6fbd26727948578e011c4248c42c62d2c
Marcelo Cardoso de Oliveira <marcelo@ivalix.com.br>
2025-04-15 21:46:59 -0300

    Update ligthController.ino

--- ligthController.ino
+++ ligthController.ino
@@ -7,7 +7,14 @@ void setup(){
 
 void loop(){
   leituraLuz = analogRead(A0);
-  int controleLed = map(leituraLuz, 0, 1023, 0, 255);
+  int controleLed = map(leituraLuz, 0,1023,255,0);
+  if(controleLed < 0){
+    controleLed = 0;
+  }
+  else if(controleLed > 255){
+    controleLed = 255;
+  }
+  Serial.println(controleLed);
   analogWrite(5,controleLed);
   
 }
