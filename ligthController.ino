ffe0419667f6b43a6f7cddaeca4e077ee368360f
Marcelo Cardoso de Oliveira <marcelo@ivalix.com.br>
2025-04-15 21:31:30 -0300

    Create ligthController.ino

---
+++ ligthController.ino
@@ -0,0 +1,13 @@
+int leituraLuz;
+
+void setup(){  
+  Serial.begin(9600);
+  pinMode(5,OUTPUT);
+}
+
+void loop(){
+  leituraLuz = analogRead(A0);
+  int controleLed = map(0,1023,0,255);
+  analogWrite(5,controleLed);
+  
+}
